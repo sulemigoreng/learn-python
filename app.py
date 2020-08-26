8a301cda148db86c887cb7e65df2d4f52c25fd94
sulemigoreng <sulemigoreng@gmail.com>
2020-08-26 10:43:18 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+# ------------------------
+# Hello World
+# ------------------------
+# print("Hello World")
+
+# ------------------------
+# Drawing Shape
+# ------------------------
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# ------------------------
+# Variables
+# ------------------------
+character_name = "John"
+character_age = "35"
+print("There once was a man named "+ character_name +", ")
+print("he was "+character_age+" years old. ")
+
+character_name = "Mike"
+character_age = "35"
+print("He really liked the name "+character_name+", ")
+print("but didn't like being "+character_age+".")
