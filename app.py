5a192af9a1390465c15b6dadd88360fdaf9dffaa
sulemigoreng <sulemigoreng@gmail.com>
2020-08-26 10:58:26 +0700

    Adding Some Code

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 # ------------------------
 # Hello World
 # ------------------------
-# print("Hello World")
+print("Hello World")
+print("\n")
 
 # ------------------------
 # Drawing Shape
 # ------------------------
-# print("   /|")
-# print("  / |")
-# print(" /  |")
-# print("/___|")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+print("\n")
 
 # ------------------------
-# Variables
+# Variables and Data Types
 # ------------------------
 character_name = "John"
 character_age = "35"
@@ -23,3 +25,18 @@ character_name = "Mike"
 character_age = "35"
 print("He really liked the name "+character_name+", ")
 print("but didn't like being "+character_age+".")
+print("\n")
+
+# ------------------------
+# Working with Strings
+# ------------------------
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print(phrase.lower() + " " +phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+print("\n")
