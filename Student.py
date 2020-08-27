655a786f04c6f89cae0c722050c52152fd487180
sulemigoreng <sulemigoreng@gmail.com>
2020-08-27 11:15:54 +0700

    Class and Object

---
+++ Student.py
@@ -0,0 +1,9 @@
+class Student:
+
+    #Constructor
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
---
+++ class-object.py
@@ -0,0 +1,7 @@
+from Student import Student  # From the Student File, import the Student class
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 3.2, False)
+
+print(student1.gpa)
+print(student2.gpa)
