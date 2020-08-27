2f80c32c6e803f0abf3f3ede776834753dd041cb
sulemigoreng <sulemigoreng@gmail.com>
2020-08-27 11:38:57 +0700

    Inheritance

---
+++ ChineseChef.py
@@ -0,0 +1,11 @@
+from Chef import Chef
+
+
+class ChineseChef(Chef):
+
+    # Override this function
+    def make_special_dish(self):
+        print("The chef makes Orange Chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes Fried Rice")
---
+++ inheritance.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_chicken()
+myChineseChef.make_fried_rice()
+myChineseChef.make_special_dish()
